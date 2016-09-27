5fc024f1c8d579f65819dda719c1210bdfa17d1a
phantamanta44 <evandalong@gmail.com>
2016-09-27 15:59:46 -0500

    Clean up imports

--- src/main/java/io/github/phantamanta44/discord4j/data/wrapper/PrivateChannel.java
+++ src/main/java/io/github/phantamanta44/discord4j/data/wrapper/PrivateChannel.java
@@ -1,10 +1,5 @@
 package io.github.phantamanta44.discord4j.data.wrapper;
 
-import java.io.File;
-import java.io.InputStream;
-
-import io.github.phantamanta44.discord4j.core.RequestQueue;
-import io.github.phantamanta44.discord4j.util.concurrent.deferred.IUnaryPromise;
 import sx.blah.discord.handle.obj.IPrivateChannel;
 
 public class PrivateChannel extends Channel {
